1f4f47969fd6c9ec338bc3ed6ccfc80b5a94aac5
trisHa-24 <mt123456trisha@gmail.com>
2024-10-22 20:53:45 +0530

    error and response handleing

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT} `);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+    
+})
 
 
 
